65058da41660d525580064b4ca2a56b5575140c6
Mirasque <obscure1321@gmail.com>
2023-11-10 11:03:31 +0300

    added model

--- RickMortyApp/Models/Character.swift
+++ RickMortyApp/Models/Character.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+// MARK: - Result
+struct Result: Codable {
+    let id: Int?
+    let name: String?
+    let status: Status?
+    let species: Species?
+    let type: String?
+    let gender: Gender?
+    let origin, location: Location?
+    let image: String?
+    let episode: [String]?
+    let url: String?
+    let created: String?
+}
+
+enum Gender: String, Codable {
+    case female = "Female"
+    case male = "Male"
+    case unknown = "unknown"
+}
+
+struct Location: Codable {
+    let name: String?
+    let url: String?
+}
+
+enum Species: String, Codable {
+    case alien = "Alien"
+    case human = "Human"
+}
+
+enum Status: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case unknown = "unknown"
+}
